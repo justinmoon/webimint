9fdc152ba66332bbc16160d49863d196ca157bf2
Justin Moon <mail@justinmoon.com>
2022-09-18 17:27:10 -0500

    Trying to compile actual fedimint client

--- webimint/src/lib.rs
+++ webimint/src/lib.rs
@@ -1,17 +1,12 @@
 use wasm_bindgen::prelude::*;
 
+use fedimint_api::db::mem_impl::MemDatabase;
+
 #[wasm_bindgen]
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn connect(left: usize, right: usize) -> usize {
+    let database = MemDatabase::new();
+    // pub async fn new(db: Box<dyn Database>, cfg_json: &str) -> anyhow::Result<Self> {
+    // let connect_cfg: WsFederationConnect = serde_json::from_str(cfg_json)?;
+    // let api = WsFederationApi::new(connect_cfg.max_evil, connect_cfg.members);
+    // let cfg: ClientConfig = api.request("/config", ()).await?;
 }
